1bb7975f1f7190c7e8eb650e7023e9b3842bcb44
FelixVR <felixvr057@gmail.com>
2025-10-27 20:32:40 -0500

    SE MOVIO

--- 8077_Grupo3_Moanso_Actividad/Form1.Designer.cs
+++ 8077_Grupo3_Moanso_Actividad/Form1.Designer.cs
@@ -33,11 +33,11 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(242, 31);
+            this.button1.Location = new System.Drawing.Point(480, 253);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(310, 110);
             this.button1.TabIndex = 0;
-            this.button1.Text = "HOLA WINSO";
+            this.button1.Text = "HOLA Arturo";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
