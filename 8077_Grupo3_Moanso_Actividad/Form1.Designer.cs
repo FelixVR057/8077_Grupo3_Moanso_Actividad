165238d0125900b80011fd8726c06ebf47993af2
Arturo Armas <arturoarmas1982@gmail.com>
2025-10-27 20:11:01 -0500

    cambiar nombre boton

--- 8077_Grupo3_Moanso_Actividad/Form1.Designer.cs
+++ 8077_Grupo3_Moanso_Actividad/Form1.Designer.cs
@@ -33,11 +33,11 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(268, 214);
+            this.button1.Location = new System.Drawing.Point(242, 31);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(310, 110);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "HOLA WINSO";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
