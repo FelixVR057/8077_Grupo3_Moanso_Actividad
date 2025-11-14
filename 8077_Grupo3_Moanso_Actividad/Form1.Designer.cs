f0aa5385997521ebda184b7b3cc943ab05f04a86
Will Valderrama Reyes <willvalderramareyes4@gmail.com>
2025-11-14 13:03:40 -0500

    se arreglo el diseño del formulario Registro Compra De Materiales

--- 8077_Grupo3_Moanso_Actividad/Form1.Designer.cs
+++ 8077_Grupo3_Moanso_Actividad/Form1.Designer.cs
@@ -79,7 +79,7 @@
             this.dgvCompra.Name = "dgvCompra";
             this.dgvCompra.RowHeadersWidth = 62;
             this.dgvCompra.RowTemplate.Height = 28;
-            this.dgvCompra.Size = new System.Drawing.Size(773, 230);
+            this.dgvCompra.Size = new System.Drawing.Size(1045, 244);
             this.dgvCompra.TabIndex = 1;
             this.dgvCompra.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvCompra_CellClick);
             // 
@@ -88,7 +88,7 @@
             this.btnAnular.BackColor = System.Drawing.Color.Red;
             this.btnAnular.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnAnular.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.btnAnular.Location = new System.Drawing.Point(197, 600);
+            this.btnAnular.Location = new System.Drawing.Point(51, 673);
             this.btnAnular.Name = "btnAnular";
             this.btnAnular.Size = new System.Drawing.Size(193, 52);
             this.btnAnular.TabIndex = 2;
@@ -100,7 +100,7 @@
             // 
             this.btnSalir.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.btnSalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSalir.Location = new System.Drawing.Point(666, 600);
+            this.btnSalir.Location = new System.Drawing.Point(311, 673);
             this.btnSalir.Name = "btnSalir";
             this.btnSalir.Size = new System.Drawing.Size(193, 52);
             this.btnSalir.TabIndex = 4;
@@ -140,25 +140,25 @@
             // 
             this.lbFecha.AutoSize = true;
             this.lbFecha.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbFecha.Location = new System.Drawing.Point(353, 113);
+            this.lbFecha.Location = new System.Drawing.Point(471, 148);
             this.lbFecha.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbFecha.Name = "lbFecha";
-            this.lbFecha.Size = new System.Drawing.Size(46, 13);
+            this.lbFecha.Size = new System.Drawing.Size(57, 17);
             this.lbFecha.TabIndex = 18;
             this.lbFecha.Text = "Fecha:";
             // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(356, 143);
+            this.dateTimePicker1.Location = new System.Drawing.Point(537, 148);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(191, 20);
+            this.dateTimePicker1.Size = new System.Drawing.Size(191, 22);
             this.dateTimePicker1.TabIndex = 17;
             // 
             // txtRuc
             // 
-            this.txtRuc.Location = new System.Drawing.Point(408, 32);
+            this.txtRuc.Location = new System.Drawing.Point(534, 40);
             this.txtRuc.Name = "txtRuc";
-            this.txtRuc.Size = new System.Drawing.Size(139, 20);
+            this.txtRuc.Size = new System.Drawing.Size(139, 22);
             this.txtRuc.TabIndex = 16;
             // 
             // lbRuc
@@ -222,9 +222,9 @@
             // 
             this.btnModificar.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.btnModificar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnModificar.Location = new System.Drawing.Point(599, 58);
+            this.btnModificar.Location = new System.Drawing.Point(799, 66);
             this.btnModificar.Name = "btnModificar";
-            this.btnModificar.Size = new System.Drawing.Size(145, 42);
+            this.btnModificar.Size = new System.Drawing.Size(193, 57);
             this.btnModificar.TabIndex = 7;
             this.btnModificar.Text = "Modificar";
             this.btnModificar.UseVisualStyleBackColor = false;
@@ -328,7 +328,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(855, 654);
+            this.ClientSize = new System.Drawing.Size(1276, 853);
             this.Controls.Add(this.lbTitulo);
             this.Controls.Add(this.pictureBoxLogo);
             this.Controls.Add(this.groupBox1);
@@ -339,7 +339,6 @@
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Registro de Compras";
-            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvCompra)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
