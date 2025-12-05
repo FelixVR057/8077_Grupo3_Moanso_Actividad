f5cb85335d7ab2430730613a6d376a28a4c754b7
Nicolas Abanto <nicabant@hotmail.com>
2025-12-05 16:49:34 -0500

    Se subio forms anteriores

---
+++ CapaLogica/LogicaMaterial.cs
@@ -0,0 +1,27 @@
+﻿using CapaDatos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaEntidad;
+
+namespace CapaLogica
+{
+    public class LogicaMaterial
+    {
+        private static readonly LogicaMaterial _instancia = new LogicaMaterial();
+        public static LogicaMaterial Instancia => _instancia;
+
+        public List<CE_Material> ListarMateriales()
+        {
+            return DaoMaterial.Instancia.ListarMateriales();
+        }
+
+        public void AgregarMaterial(CE_Material mat)
+        {
+            DaoMaterial.Instancia.InsertarMaterial(mat);
+        }
+
+    }
+}
